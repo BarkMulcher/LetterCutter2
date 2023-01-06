b3da018b509c2d63bd75e7b7401ca667e7ff08a1
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-06 14:20:19 -0500

    reorg & add pgs for GH pages test

---
+++ app/memorials/[id]/page.js
@@ -0,0 +1,4 @@
+/* /app/products/[id]/page.js */
+export default function MemorialWithId({ params }) {
+    return (<h1> Individual product with id: {params.id} </h1>
+    )}
---
+++ app/memorials/page.js
@@ -0,0 +1,5 @@
+export default function MemorialIndex() {
+    return (
+        <h1> Memorials Page</h1>
+    )
+}
