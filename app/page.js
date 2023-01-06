e55b8de4ee2c5bf58f57a13c41bf0a58935464a5
BarkMulcher <lucas.c.haskell@gmail.com>
2023-01-06 15:20:02 -0500

    build w/o export for deploymnt

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 export default function Index() {
     return (
+        <>
         <h1> The Home Page</h1>
+        <nav>
+            <Link href='/about'>About</Link>
+            <Link href='/memorials'>Memorials</Link>
+        </nav>
+        </>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/LetterCutter2",
+  // this is important:
+    // changes to this returned the site to showing readme as the page
+  assetPrefix: isProd ? "https://BarkMulcher.github.io/LetterCutter2" : '',
   reactStrictMode: true,
   experimental: {
     appDir: true,
-  }
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, {
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/about': { page: '/about/about' },
+  //     '/memorials': { page: 'memorials/memorials'},
+  //   }
+  // },
+}
